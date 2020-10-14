7363a6a8b9efff31034f8d376dd401d811c5b0b6
dragun_shot0 <dragunshot0@gmail.com>
2020-10-14 10:39:01 +0900

    -Add login -Add css grid component

---
+++ first_prob/src/App.js
@@ -0,0 +1,96 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="App1">
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+        <div class="item">1</div>
+      </div>
+      <div className="App2">
+        <div class="item header two">2</div>
+        <div class="item header two">2</div>
+        <div class="item header one">1</div>
+
+        <div class="item main">1</div>
+        <div class="item main">1</div>
+        <div class="item main">1</div>
+        <div class="item main">1</div>
+        <div class="item aside">1</div>
+
+        <div class="item main">1</div>
+        <div class="item main">1</div>
+        <div class="item main">1</div>
+        <div class="item main">1</div>
+        <div class="item aside">1</div>
+
+        <div class="item main">1</div>
+        <div class="item main">1</div>
+        <div class="item main">1</div>
+        <div class="item main">1</div>
+        <div class="item aside">1</div>
+
+        <div class="item footer">1</div>
+        <div class="item footer">1</div>
+        <div class="item footer">1</div>
+        <div class="item footer">1</div>
+        <div class="item footer">1</div>
+      </div>
+      <div className="App3">
+        <div class="item header square">2</div>
+        <div class="item header one">1</div>
+        <div class="item header one">1</div>
+        <div class="item header one">1</div>
+
+        <div class="item main">1</div>
+        <div class="item main">1</div>
+        <div class="item aside">1</div>
+
+        <div class="item main square">2</div>
+        <div class="item main">1</div>
+        <div class="item main">1</div>
+        <div class="item aside">1</div>
+
+        <div class="item main">1</div>
+        <div class="item main">1</div>
+        <div class="item aside">1</div>
+
+        <div class="item footer">1</div>
+        <div class="item footer">1</div>
+        <div class="item footer">1</div>
+        <div class="item footer">1</div>
+        <div class="item footer">1</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
